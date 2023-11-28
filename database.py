b7e940cfe178523cb12a493474e1c54d90c55c32
chinmayujoshi <152302295+chinmayujoshi@users.noreply.github.com>
2023-11-28 20:28:21 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+load_dotenv()
+engine = create_engine(os.getenv('DATABASE_CONNECTION_STRING'))
+
+
+def addScore(timestamp, score):
+    query = text("INSERT INTO log (timestamp, score) VALUES (:timestamp, :score)")
+    params = {'timestamp': timestamp, 'score': score}
+
+    try:
+        print("Executing SQL Query:")
+        print(query)
+        print("Parameters:")
+        print(params)
+
+        with engine.connect() as conn:
+            result = conn.execute(query.bindparams(**params))
+            conn.commit() 
+
+        print("Insert successful.")
+        return result
+
+    except Exception as e:
+        print("Error during insertion:")
+        print(str(e))
+        raise  # Re-raise the exception after logging it
+
+def its_a_date(selected_value):
+    query = text("INSERT INTO date (date) VALUES (:date)")
+    params = {'date': selected_value}
+    with engine.connect() as conn:
+            result = conn.execute(query.bindparams(**params))
+            conn.commit() 
